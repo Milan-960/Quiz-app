3665528d91d62fc41483405d802856569888d118
Milan Sachani <79278557+Milan-960@users.noreply.github.com>
2022-01-31 08:26:14 +0100

    redux state changed abd API added 🔥

--- src/Pages/Final.js
+++ src/Pages/Final.js
@@ -1,13 +1,31 @@
-import React from "react";
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
 
-function Final() {
+import { handleAmountChnage, handleScoreChnage } from "../Redux/action";
+
+const Final = () => {
+  const disptach = useDispatch();
+  const history = useHistory();
+  const { score } = useSelector((state) => state);
+
+  const handleBackToSettings = () => {
+    disptach(handleScoreChnage(0));
+    disptach(handleAmountChnage(50));
+    history.push("/");
+  };
+
   return (
-    <div>
-      <h1>setting</h1>
-    </div>
+    <Box mt={30}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>
+        Final Score {score}
+      </Typography>
+      <Button onClick={handleBackToSettings} variant="outlined">
+        back to settings!
+      </Button>
+    </Box>
   );
-}
+};
 
 export default Final;
-
-<h1>setting</h1>;
