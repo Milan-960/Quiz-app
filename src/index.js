16f7ddd2c80ea9f739ea81fdfd3718fe21f31522
Milan Sachani <79278557+Milan-960@users.noreply.github.com>
2022-02-14 20:55:51 +0100

    service worker added 😎

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import { Provider } from "react-redux";
 import store from "./Redux/store";
